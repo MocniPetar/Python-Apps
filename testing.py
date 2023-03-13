50bda8c7d4e3913c16215635d91f39d321661721
Pier Strbad <107961505+MocniPetar@users.noreply.github.com>
2023-03-13 18:06:00 +0100

    App for store managers

---
+++ testing.py
@@ -0,0 +1,14 @@
+items_list = [  ["Banana", "1.00"],
+                ["Cokolada", "2.00"],
+                ["Igracka", "20.00"],
+                ["Jabuka", "0.50"],
+                ["Jaja", "0.75"],
+                ["Kruh", "1.20"],
+                ["Mlijeko", "2.50"],
+                ["Sladoled", "3.00"],
+                ["Vino", "10.00"],
+                ["Voda", "1.50"]
+                                        ]
+
+for i in items_list:
+    print(f'{i[0]} {i[1]}')
